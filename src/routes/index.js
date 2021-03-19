63416256048c22d7f257e03d56828f589eb15444
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2021-03-18 20:12:22 -0600

    html created, "autor" get modified, post deleted

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,35 +14,11 @@ const toneAnalyzer = new ToneAnalyzerV3({
 
 router.route('/autor').get((req, res) => {
   alumno = `MMM`
-  servicio = "Cloud Foundry en IBM Cloud"
+  servicio = "ECS en AWS"
   res.json({
     alumno,
     servicio
   });
-})
-
-
-router.route('/').post((req, res) => {
-  //Content-Type: application/json
-  if(req.body.text){
-    const toneParams = {
-      toneInput: { 'text': req.body.text },
-      contentType: 'application/json',
-    };
-    toneAnalyzer.tone(toneParams)
-    .then(toneAnalysis => {
-      res.json(toneAnalysis.result);
-    })
-    .catch(err => {
-      console.log('error:', err);
-    });
-  }else{
-    //En caso de error se debe regresar una salida indicando el error (Por ejemplo si recibes un JSON sin el tag esperado).
-    res.status(400).json({
-      error: "error, bad request: \"Missing 'text' field in JSON\""
-  })
-  }
-  
 })
 
 
